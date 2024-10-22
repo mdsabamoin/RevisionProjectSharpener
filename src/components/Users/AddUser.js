e2093e5d1f236b80344ac71a6afa01669088affc
unknown <sabamoin419@gmail.com>
2024-10-22 16:45:14 +0530

    collecting user input revision

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,24 +1,34 @@
-// Write your code at relevant places in the code below:
-
-import React from "react";
-import Card from  "./card.js";
+import React,{useState} from "react";
+import Card from "./card";
+import Button from "./Button";
 import "./AddUser.css";
+
 const AddUser = () => {
+  const [name , setName] = useState("");
+  const [age , setAge] = useState("");
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(name,age);
+    // console.log(age);
+    setName("");
+    setAge("");
   };
-
+const inputname = (event) =>{
+  setName(event.target.value);
+}
+const inputage = (event) =>{
+  setAge(event.target.value);
+}
   return (
     <Card className="input">
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" value={name} onChange={inputname}/>
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number"  value={age} onChange={inputage} />
+        <Button type="submit">Add User</Button>
+      </form>
     </Card>
-    
   );
 };
 
